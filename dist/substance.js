f56f322af69f27faa7b461c38cdcd6de73c5865f
Michael Aufreiter <michael@substance.io>
2013-10-24 18:14:17 +0200

    Update bundle.

--- dist/substance.js
+++ dist/substance.js
@@ -5800,7 +5800,6 @@ var Article = function(options) {
   // --------
 
   options.schema = util.deepclone(Document.schema);
-
   options.schema.id = "substance-article";
   options.schema.version = "0.2.0";
 
@@ -5813,7 +5812,6 @@ var Article = function(options) {
   _.each(Article.annotations, function(aType, key) {
     options.schema.types[key] = aType;
   });
-
 
   // Merge in node types
   _.each(Article.nodeTypes, function(node, key) {
@@ -6031,6 +6029,157 @@ Article.indexes = {
     "properties": ["node"]
   }
 };
+
+
+// From article definitions generate a nice reference document
+// --------
+//
+
+var ARTICLE_DOC_SEED = {
+  "id": "article",
+  "nodes": {
+    "document": {
+      "type": "document",
+      "id": "document",
+      "views": [
+        "content",
+        "info"
+      ],
+      "title": "The Anatomy of a Substance Article",
+      "collaborators": ["collaborator_1", "collaborator_2"],
+      "guid": "lens_article"
+    },
+
+    "content": {
+      "type": "view",
+      "id": "content",
+      "nodes": [
+        "cover",
+      ]
+    },
+
+    "cover": {
+      "id": "cover",
+      "type": "cover",
+      "collaborators": ["collaborator_1", "collaborator_2"]
+    },
+
+    "collaborator_1": {
+      "id": "collaborator_1",
+      "type": "collaborator",
+      "name": "Michael Aufreiter"
+    },
+
+    "collaborator_2": {
+      "id": "collaborator_2",
+      "type": "collaborator",
+      "name": "Oliver Buchtala"
+    }
+  }
+};
+
+Article.describe = function() {
+  var doc = new Article({seed: ARTICLE_DOC_SEED});
+
+  var id = 0;
+
+  _.each(Article.nodeTypes, function(nodeType, key) {
+    if (key === "composite") return;
+    nodeType = nodeType.Model;
+
+    // Create a heading for each node type
+    var headingId = "heading_"+nodeType.type.id;
+
+    doc.create({
+      id: headingId,
+      type: "heading",
+      content: nodeType.description.name,
+      level: 1
+    });
+
+    // Turn remarks and description into an introduction paragraph
+    var introText = nodeType.description.remarks.join(' ');
+    var introId = "text_"+nodeType.type.id+"_intro";
+
+    doc.create({
+      id: introId,
+      type: "text",
+      content: introText,
+    });
+
+
+    // Show it in the content view
+    doc.show("content", [headingId, introId], -1);
+
+    // Include property description
+    // --------
+    //
+
+    doc.create({
+      id: headingId+"_properties",
+      type: "text",
+      content: nodeType.description.name+ " uses the following properties:"
+    });
+
+    doc.show("content", [headingId+"_properties"], -1);
+
+    var items = [];
+
+    _.each(nodeType.description.properties, function(propertyDescr, key) {
+
+      var listItemId = "text_" + (++id);
+      doc.create({
+        id: listItemId,
+        type: "text",
+        content: key +": " + propertyDescr
+      });
+
+      // Create code annotation for the propertyName
+      doc.create({
+        "id": id+"_annotation",
+        "type": "code",
+        "path": [listItemId, "content"],
+        "range":[0, key.length]
+      });
+
+      items.push(listItemId);
+    });
+
+    // Create list
+    doc.create({
+      id: headingId+"_property_list",
+      type: "list",
+      items: items,
+      ordered: false
+    });
+
+    // And show it
+    doc.show("content", [headingId+"_property_list"], -1);
+
+    // Include example
+    // --------
+
+    if (nodeType.example) {
+      doc.create({
+        id: headingId+"_example",
+        type: "text",
+        content: "Here's an example:"
+      });
+
+      doc.create({
+        id: headingId+"_example_codeblock",
+        type: "codeblock",
+        content: JSON.stringify(nodeType.example, null, '  '),
+      });
+      doc.show("content", [headingId+"_example", headingId+"_example_codeblock"], -1);
+    }
+  });
+
+  return doc;
+};
+
+
+
 
 Article.Prototype.prototype = Document.prototype;
 Article.prototype = new Article.Prototype();
@@ -23508,10 +23657,7 @@ SubstanceView.prototype = new SubstanceView.Prototype();
 module.exports = SubstanceView;
 
 },{"substance-application":58,"substance-util":167,"underscore":172}],176:[function(require,module,exports){
-
-// not implemented
-// The reason for having an empty file and not throwing is to allow
-// untraditional implementation of this module.
+// nothing to see here... no file methods for the browser
 
 },{}]},{},[1])
 ;
